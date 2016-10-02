8b9384f000536604a9bc7226e5d671e3f2a6abbc
moreC <longbaobao_today@163.com>
2016-10-02 20:52:28 +0800

    a sample python web

---
+++ sample/python/code.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+import web
+
+urls = ( '/', 'index','/hello', 'hello', '/test*', 'test')
+
+def read_html(htmlfile):
+    return open(htmlfile, 'rb').read()
+
+class index:
+    def GET(self):
+        return read_html('index.html')
+
+class hello:
+    def GET(self):
+        return "<h1><font color='red'>Hello World!</font><h1>"
+        # return read_html('index.html')
+
+class test():
+    def GET(self):
+        user_data = web.input(FirstName="no", LastName="no")
+        return "<h1>" + user_data.FirstName + "<br>" + user_data.LastName + "</h1>"
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
